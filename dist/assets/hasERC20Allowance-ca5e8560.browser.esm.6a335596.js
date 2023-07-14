e71dfe4d4e2b7202ba7b3582286d0a1021a06869
flyingbeat <69470817+flyingbeat@users.noreply.github.com>
2023-07-14 17:35:46 +0200

    adding dist 2

--- dist/assets/hasERC20Allowance-ca5e8560.browser.esm.6a335596.js
+++
@@ -1,1 +0,0 @@
-import{A as c,aJ as d}from"./index.2db048f1.js";async function w(a,e,n){const r=a.getProvider(),t=new c(r,e,d,{}),s=await a.getSignerAddress(),o=a.readContract.address;return(await t.readContract.allowance(s,o)).gte(n)}export{w as h};
