35bb45f556509d613a35f508ed9fcde86a6bb941
flyingbeat <69470817+flyingbeat@users.noreply.github.com>
2023-07-14 18:01:22 +0200

    deployment

---
+++ assets/hasERC20Allowance-ca5e8560.browser.esm.8a8b6a6d.js
@@ -0,0 +1,1 @@
+import{A as c,aJ as d}from"./index.cc1b9eaa.js";async function w(a,e,n){const r=a.getProvider(),t=new c(r,e,d,{}),s=await a.getSignerAddress(),o=a.readContract.address;return(await t.readContract.allowance(s,o)).gte(n)}export{w as h};
